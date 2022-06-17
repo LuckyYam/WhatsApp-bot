d3ff604576eb62d820bd24df1bf4f2821ecc0596
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-06-17 20:37:35 +0500

    Add files via upload

---
+++ src/Commands/Utils/Carbon.js
@@ -0,0 +1,40 @@
+const Command = require('../../Structures/Command')
+const Message = require('../../Structures/Message')
+const Carbon = require('unofficial-carbon-now')
+
+module.exports = class command extends Command {
+    constructor() {
+        super('carbon', {
+            description: 'Will send you the given code as carbon image.',
+            category: 'utils',
+            usage: 'carbon hello world',
+            aliases: ['code'],
+            exp: 20
+        })
+    }
+
+    /**
+     * @param {Message} M
+     * @returns {Promise<void>}
+     */
+
+    execute = async (M, { context }) => {
+     const texas = context.trim()
+
+  if (!context)
+        return void M.reply('Provide the code you want as carbon image, Baka!')
+
+ try {
+     const carbon = new Carbon.createCarbon()
+         .setCode(texas)
+         .setBackgroundColor(this.helper.utils.generateRandomHex())
+         .setExportSize(3)
+       
+     const buffer =  await Carbon.generateCarbon(carbon) 
+     await M.reply(buffer, 'image')
+
+ } catch(err) {
+    console.log(`Error Occurred`) 
+    return void (await M.reply(`_Error can't generate image, Baka!_`));
+             }
+}}
---
+++ src/Commands/Utils/Emoji.js
@@ -0,0 +1,51 @@
+const Command = require('../../Structures/Command')
+const Message = require('../../Structures/Message')
+const {Sticker, createSticker, StickerTypes} = require('wa-sticker-formatter')
+
+module.exports = class command extends Command {
+    constructor() {
+        super('emoji', {
+            description: 'Will send you a given emoji into sticker.',
+            category: 'utils',
+            usage: 'emoji 😗',
+            aliases: ['emoj','ejs'],
+            exp: 20
+        })
+    }
+
+    /**
+     * @param {Message} M
+     * @returns {Promise<void>}
+     */
+
+    execute = async (M, { context }) => {
+    const code = context.trim()
+    
+    let ent = `${code}`
+    let hex = ent.codePointAt(0).toString(16)
+    let emo = String.fromCodePoint("0x"+hex);
+
+  if (!context)
+        return void M.reply('Provide the Emoji you want as sticker, Baka!')
+
+  try {
+    const b = await this.helper.utils.getBuffer(`https://emojiapi.dev/api/v1/${hex}/512.webp`)
+    
+ let sticker = new Sticker(b, {
+    pack: "Emoji Sticker By",
+    author: "${this.helper.config.name} 🌟",
+    type: StickerTypes.FULL, 
+    categories: ['🤩', '🎉'],
+    id: '12345', 
+    quality: 50, 
+    background: 'transparent'
+    })
+
+    const buffer = await sticker.toBuffer()
+    await M.reply(buffer, 'sticker')
+
+} catch(err) {
+    console.log(`Emoji Error Occurred`) 
+    return void (await M.reply(`*Invalid Input, Baka!*`));
+             }
+}}
